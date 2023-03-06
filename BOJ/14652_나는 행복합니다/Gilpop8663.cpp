e688534b746d075a043d49faf6c247a2c333dd4a
Gilpop8663 <wolfye@naver.com>
2023-03-06 22:05:12 +0900

    Add:14652:나는 행복합니다

---
+++ BOJ/14652_나는 행복합니다/Gilpop8663.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, k;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m >> k;
+
+    cout << k / m << " " << k % m;
+}
