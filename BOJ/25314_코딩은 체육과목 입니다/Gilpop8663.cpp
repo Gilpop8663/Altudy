22f4fd135eb056b26f51c552e2d38197294c7939
gilpop8663 <wolfye@naver.com>
2023-03-08 04:59:54 +0900

    Add:25314_코딩은 체육과목 입니다

---
+++ BOJ/25314_코딩은 체육과목 입니다/Gilpop8663.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    int count = n / 4 - 1;
+
+    for (int i = 0; i < count; i++)
+    {
+        cout << "long ";
+    }
+    cout << "long int";
+}
