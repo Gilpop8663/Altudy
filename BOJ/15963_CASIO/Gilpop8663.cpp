0f45f6be8ef97ca6a9eb04ce185e8faef1b1fcc7
gilpop8663 <wolfye@naver.com>
2023-03-08 23:58:13 +0900

    Add:15963_CASIO

---
+++ BOJ/15963_CASIO/Gilpop8663.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n, m;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    if (n == m)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+}
