9aa1a83060f74574409dcd74a7fcb29d1bd6444d
gilpop8663 <wolfye@naver.com>
2023-03-14 23:42:20 +0900

    Add:9316_Hello Judge

---
+++ BOJ/9316_Hello Judge/Gilpop8663.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n, m;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Hello World, Judge " << i << "!"
+             << "\n";
+    }
+}
