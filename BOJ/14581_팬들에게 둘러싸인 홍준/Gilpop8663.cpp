b4cbc09c3c1cf9776b51d16f05b11c27702fe70b
gilpop8663 <wolfye@naver.com>
2023-03-27 22:15:02 +0900

    Add:14581_팬들에게 둘러싸인 홍준

---
+++ BOJ/14581_팬들에게 둘러싸인 홍준/Gilpop8663.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int n;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string str;
+    cin >> str;
+
+    cout << ":fan:"
+         << ""
+         << ":fan:"
+         << ""
+         << ":fan:"
+         << "\n";
+    cout << ":fan:"
+         << ":" << str << ":"
+         << ":fan:"
+         << "\n";
+    cout << ":fan:"
+         << ""
+         << ":fan:"
+         << ""
+         << ":fan:"
+         << "\n";
+}
