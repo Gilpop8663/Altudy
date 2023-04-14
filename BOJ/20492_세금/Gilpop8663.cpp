82330f293c803a6d8b0d1209107db188e7f0e355
Gilpop8663 <wolfye@naver.com>
2023-04-14 21:34:38 +0900

    Add:20492_세금

---
+++ BOJ/20492_세금/Gilpop8663.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    cout << n * 78 / 100 << "\n";
+
+    cout << n * 8 / 10 + (n * 156 / 1000);
+}
