fd32eedc56de22d1ed68c71510b6fde61079e5f6
gilpop8663 <wolfye@naver.com>
2023-04-29 21:13:31 +0900

    Add: 20254_Site Score

---
+++ BOJ/20254_Site Score/Gilpop8663.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c, d;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a >> b >> c >> d;
+
+    int ans = 56 * a + 24 * b + 14 * c + 6 * d;
+
+    cout << ans;
+}
